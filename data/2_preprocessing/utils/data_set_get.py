4eedf2df702e10e9adcccf660c4a847822b3406a
jenajeong <skg4660@gmail.com>
2025-09-04 14:36:47 +0900

    [DATA]FIX: json키 경로를 읽지 못하는 문제 수정

--- data/2_preprocessing/utils/data_set_get.py
+++ data/2_preprocessing/utils/data_set_get.py
@@ -7,7 +7,7 @@ from google.oauth2.service_account import Credentials
 
 # 설정 파일에서 key_path, sheet_id1, sheet_id2 불러오기
 def load_config(json_filename="import_info2.json"):
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     json_path = os.path.join(BASE_DIR, json_filename)
 
     with open(json_path, "r", encoding="utf-8") as f:
